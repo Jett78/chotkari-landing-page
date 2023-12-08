f43777059dc3836f9828caf149897998ee362712
Bikash-Karki <bikash2000karki@gmail.com>
2023-12-08 10:45:55 +0545

    spelling mistake fix

--- src/components/JoinWaitlist.js
+++ src/components/JoinWaitlist.js
@@ -56,7 +56,7 @@ export default function JoinWaitlist() {
             <section className="ck-join-waitlist-section-container" id='join-waitlist'>
                 <div className="ck-join-waitlist-inner-container">
                     <div className="ck-join-waitlist-info ck-giveaway-info">
-                        <h1 className="custom-heading-giveaway">Join our Waitling List</h1>
+                        <h1 className="custom-heading-giveaway">Join our Waiting List</h1>
                         <p className="mt-5 ck-giveaway-p">
                             Indulge yourself into a world of greatness where you'll be a part of a community that values information, connection, and the joy of reading.
                             Don't miss out on the opportunity to experience the future of news/article delivery first-hand, as we are giving away merchandise to a selected few.
