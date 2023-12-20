1f91b019fa3036dfb2a7aceb6149a25d8facfe42
Bikash-Karki <bikash2000karki@gmail.com>
2023-12-20 13:41:14 +0545

    add:New GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import './globals.css'
 import Head from 'next/head'
+import Script from 'next/script'
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -21,22 +22,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-      <head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){
-                w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});
-                var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-                j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-K6CLDFQS');
-            `,
-          }}
-        />
-      </head>
+      <Head>
+        <Script src={`https://www.googletagmanager.com/gtag/js?id=GTM-K6CLDFQS`}
+          strategy="afterInteractive" />
+        <Script id='google-analytics' strategy='afterInteractive'>{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag() { dataLayer.push(arguments); }
+          gtag('js', new Date()); 
+          gtag('config', 'G-270H1VZXVW');
+        `}</Script>
+
+
+      </Head>
 
 
       <body className={poppins.className}>{children}
