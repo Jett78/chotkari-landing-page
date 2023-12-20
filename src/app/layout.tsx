da9d322851ed4ca8809d871ba10a640db12de8cc
Bikash-Karki <bikash2000karki@gmail.com>
2023-12-20 12:31:22 +0545

    add:Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import './globals.css'
+import Head from 'next/head'
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -20,11 +21,29 @@ export default function RootLayout({
   return (
     <html lang="en">
       <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-K6CLDFQS');
+            `,
+          }}
+        />
+      </head>
 
 
       <body className={poppins.className}>{children}
 
         <script src="https://player.vimeo.com/api/player.js"></script>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K6CLDFQS"
+          height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
       </body>
     </html>
   )
