80d7f4a39eaf9947c5587ed0734e93acad983381
Jeet <jetdeula@gmail.com>
2024-06-09 08:27:49 +0545

    new images added

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div className='grid grid-cols-7 gap-2'>
+        <img src="./screens/image 53.png" alt="screen" className='mt-[20em]' />
+        <img src="./screens/image 54 (1).png" alt="screen" className='mt-[14em]' />
+        <img src="./screens/image 55.png" alt="screen" className='mt-[8em]' />
+        <img src="./screens/image 51.png" alt="screen" />
+        <img src="./screens/image 52 (1).png" alt="screen" className='mt-[8em]'  />
+        <img src="./screens/image 57.png" alt="screen"  className='mt-[14em]'/>
+        <img src="./screens/image 56.png" alt="screen" className='mt-[20em]' />
+    </div>
+  )
+}
+
+export default HeroSection
